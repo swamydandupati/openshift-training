e6314c7f0ffe804058a97fea6af7971fa4232d44
swamydandupati <104054787+swamydandupati@users.noreply.github.com>
2023-07-19 22:02:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,5 @@
 
 print "helllooooooooooooooooo worlddddddddddddddd......\n";
 
-print "People may hear your words, but they feel your attitude. .\n";
-
-print "People may hear your words, but they feel your attitude. .\n";
-
-print "helllooooooooooooooooo worlddddddddddddddd......\n";
-print "helllooooooooooooooooo worlddddddddddddddd......\n";
-
 
 ?>
